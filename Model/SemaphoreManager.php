6a8383b23900199bf36dd3d8e26f7f961b5a0ca1
Stefan Zerkalica <zerkalica@gmail.com>
2012-08-03 18:36:14 +0400

    updates

---
+++ Model/SemaphoreManager.php
@@ -0,0 +1,29 @@
+<?php
+namespace Millwright\SemaphoreBundle\Model;
+
+use Symfony\Component\Security\Acl\Domain\ObjectIdentity;
+use Symfony\Component\Security\Acl\Domain\ObjectIdentityInterface;
+
+use Millwright\Semaphore\Model\SemaphoreManager as SemaphoreManagerBase;
+
+/**
+ * Semaphore manager extender
+ */
+class SemaphoreManager extends SemaphoreManagerBase
+{
+    /**
+     * {@inheritDoc}
+     *
+     * @param string|ObjectIdentityInterface|object $key
+     */
+    public function acquire($key)
+    {
+        if (is_object($key)) {
+            $key = $key instanceof ObjectIdentityInterface
+                ? (string) $key
+                : (string) ObjectIdentity::fromDomainObject($key);
+        }
+
+        return parent::acquire($key);
+    }
+}
